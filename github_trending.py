ed4eeb10c5b19f8aa84b67cf1277d424937ed8ba
EVGENY <evro23x@mail.ru>
2017-05-22 21:20:14 +0300

    save code and 50% task complete

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,18 @@
+import requests
+from datetime import datetime, timedelta
+from pprint import *
 
 
 def get_trending_repositories(top_size):
-    pass
+    url = 'https://api.github.com/search/repositories'
+    prepare_date = 'created:>={}'.format((datetime.now() - timedelta(days=7)).strftime('%Y-%m-%d'))
+    payload = {'sort': 'stars', 'q': prepare_date, 'order': 'desc', 'per_page': top_size, 'page': '1'}
+    r = requests.get(url, params=payload)
+    pprint(r.json())
 
 
 def get_open_issues_amount(repo_owner, repo_name):
     pass
 
 if __name__ == '__main__':
-    pass
+    get_trending_repositories(2)
